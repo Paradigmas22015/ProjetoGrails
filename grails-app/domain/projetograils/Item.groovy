d5959f844c57aaaab1a11b99981a2d8c1d506270
BeatrizFerreira <beatriz.216@hotmail.com>
2015-09-06 10:12:54 -0300

    Atributos de categoria e tamanho como obrigatorios

--- grails-app/domain/projetograils/Item.groovy
+++ grails-app/domain/projetograils/Item.groovy
@@ -14,8 +14,10 @@ class Item {
         nome nullable:false, blank:false, maxSize:128
         valor nullable:false, blank:false
         valor (matches: "[0-9]+\\.[0-9]{2}")
-        descricao  nullable:true, blank:true, maxSize:1000
+        descricao  nullable:false, blank:true, maxSize:1000
         imagem nullable:true, maxSize:65536
+        categoria nullable:false
+        tamanho nullable:false
     }
 
     static mapping = {
